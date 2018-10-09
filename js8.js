deccb62bd14e54d4b4aba495d9ad41f002586154
AngelaMallet <angelamallet@gmail.com>
2018-10-09 11:33:40 +0200

    ex8

---
+++ js8.js
@@ -0,0 +1,11 @@
+document.getElementById('submit').addEventListener("click", function(){
+var age = document.getElementById('age').value;
+if (age >= 18){
+
+  alert("Vous êtes majeur!");
+}
+  else {
+    alert("Vous êtes mineur!");
+  }
+}
+)
